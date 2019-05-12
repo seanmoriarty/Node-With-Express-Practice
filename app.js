be6ccd64c2b7a44909387b641a19b34b9a083689
Sean Moriarty <smoriarty81@gmail.com>
2019-05-12 19:48:36 -0400

    Break out routes, add route filtering

--- app.js
+++ app.js
@@ -3,24 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use('admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    console.log('this always runs');
-    next();
-});
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express!</h1>');
+    res.status(404).send('<h1>Page not found.');
 });
 
 app.listen(3000, console.log("listening on 3000"));
