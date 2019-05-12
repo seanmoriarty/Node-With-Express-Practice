c9ba6ebd8804eb151c8c8d48d339e1501a8cf6a6
Sean Moriarty <smoriarty81@gmail.com>
2019-05-12 17:21:33 -0400

    change to using express

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.use( (req, res, next) => {
+    console.log("in the middleware");
+    next();
+});
+
+app.use( (req, res, next) => {
+    console.log("in another middleware");
+    res.send('<h1>Hello From Express!</h1>');
+});
+
+app.listen(3000);
