436726dc25b4308ea7db60bcc2759d08554bec73
Sean Moriarty <smoriarty81@gmail.com>
2019-05-12 19:07:18 -0400

    added body-parser and routes folder

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use((req, res, next) => {
     console.log('this always runs');
@@ -8,11 +11,16 @@ app.use((req, res, next) => {
 });
 
 app.use('/add-product', (req, res, next) => {
-    res.send('<h1>Add Product</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
     res.send('<h1>Hello From Express!</h1>');
 });
 
-app.listen(3000);
+app.listen(3000, console.log("listening on 3000"));
