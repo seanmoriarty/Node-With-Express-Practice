5220ad49a1e2505b04dd648a7ef9d4fb0661ce5a
Sean Moriarty <smoriarty81@gmail.com>
2019-05-12 20:35:24 -0400

    Add HTMl files (views) and use sendFile and __dirname to serve up the HTML

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,18 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
-router.get('/admin/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+router.get('/add-product', (req, res, next) => {
+    console.log('admin');
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    //__dirname is absolute path on OS to the project folder it's used in(routes in this case)
+    //The next attribute will be in this case ../ to go up one level
+    //The next attribute is the folder, then the file.
 });
 
-router.post('/admin/add-product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
