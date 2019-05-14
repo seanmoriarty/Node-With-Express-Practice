fa78f42c93627a22f69cd61278e4db7dd654a457
Sean Moriarty <smoriarty81@gmail.com>
2019-05-13 20:25:49 -0400

    Add styling

--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,15 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
     console.log('admin');
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
-    //__dirname is absolute path on OS to the project folder it's used in(routes in this case)
-    //The next attribute will be in this case ../ to go up one level
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    //rootDir is a helper with the absolute path on OS to the project folder it's used in(routes in this case)
+    //The next attribute will be in this case .. to go up one level
     //The next attribute is the folder, then the file.
 });
 
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     //__dirname is absolute path on OS to the project folder it's used in(routes in this case)
     //The next attribute will be in this case ../ to go up one level
     //The next attribute is the folder, then the file.
