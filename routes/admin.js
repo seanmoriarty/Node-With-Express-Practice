6ebad5d98e3210b04f3e207c380e9db9568d8f86
Sean Moriarty <smoriarty81@gmail.com>
2019-06-09 21:23:34 -0400

    Added EJS

--- routes/admin.js
+++ routes/admin.js
@@ -6,9 +6,11 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    console.log('admin');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-Product', {pageTitle: 'Add Product'});
+    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     //rootDir is a helper with the absolute path on OS to the project folder it's used in(routes in this case)
     //The next attribute will be in this case .. to go up one level
     //The next attribute is the folder, then the file.
@@ -16,7 +18,9 @@ router.get('/add-product', (req, res, next) => {
 
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
+    products.push({title: req.body.title});
     res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
--- routes/shop.js
+++ routes/shop.js
@@ -4,10 +4,22 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    const products = adminData.products;
+    res.render('shop', {
+        prods: products, 
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+    });
+    //console.log(adminData.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
     //__dirname is absolute path on OS to the project folder it's used in(routes in this case)
     //The next attribute will be in this case ../ to go up one level
     //The next attribute is the folder, then the file.
